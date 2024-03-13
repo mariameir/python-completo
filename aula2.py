7d24e2c6f3a73dbe9fe5d768f46b90c041e5a853
Maria Meir <pigyarx@gmail.com>
2024-03-13 16:40:32 -0400

    estrutura das aulas

---
+++ aula2.py
@@ -0,0 +1,8 @@
+# Python de tipagem Dinamica / Forte (significa que ele já sabe o tipo de dado
+# que estamos passando para ele)
+
+# escape
+print("maria \"meir\"")
+
+# r
+print(r"maria \"meir\"")
