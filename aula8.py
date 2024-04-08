1edf69a49cfd40b9bc9f47f0e11390078dd14ad5
Maria Meir <pigyarx@gmail.com>
2024-04-08 09:42:15 -0400

    exercicios variaveis

---
+++ aula8.py
@@ -0,0 +1,12 @@
+nome = 'Maria Meir'
+sobrenome = "Souza"
+idade = 25
+ano_nascimento = 1998
+maior_de_idade = idade >= 18
+altura_metro = '154m'
+print('Nome:', nome)
+print('Sobrenome:', sobrenome)
+print('Idade', idade)
+print('Ano de nascimento:', ano_nascimento)
+print('É maior de idade?', maior_de_idade)
+print('Altura em metros:', altura_metro)
