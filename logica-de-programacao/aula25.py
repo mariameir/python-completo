2405e549cacda4bf5bf503d1cb2b404e559d59d4
Maria Meir <pigyarx@gmail.com>
2024-04-12 13:42:18 -0400

    interpolação

---
+++ logica-de-programacao/aula25.py
@@ -0,0 +1,13 @@
+'''
+Interpolação básica de strings
+s - string
+d e i - int
+f - float
+x e X - hexadecimal 
+'''
+
+nome = 'maria meir'
+preco = 12.435
+
+variavel = '%s, o preço é R$%.2f' % (nome, preco)
+print(variavel)
