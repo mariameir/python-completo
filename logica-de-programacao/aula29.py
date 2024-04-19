fb76903f8550a16fde486e15bd9e200c7ec94fd1
Maria Meir <pigyarx@gmail.com>
2024-04-19 15:13:39 -0400

    try execept

---
+++ logica-de-programacao/aula29.py
@@ -0,0 +1,10 @@
+"""
+Introdução ao try/execept
+try -> tentar executar o codigo
+execept -> ocorreu algum erro ao executar o codigo
+"""
+
+numero = input('vou dobrar o numero que vc digitar: ')
+numero_float = float(numero)
+
+print(f'o dobro e {numero} é {numero_float*2:.0f}')
