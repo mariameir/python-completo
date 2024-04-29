a430aa8b6addd1921c8096c013df957daa80a971
Maria Meir <pigyarx@gmail.com>
2024-04-29 15:12:29 -0400

    aula 29

--- logica-de-programacao/aula29.py
+++ logica-de-programacao/aula29.py
@@ -5,6 +5,17 @@ execept -> ocorreu algum erro ao executar o codigo
 """
 
 numero = input('vou dobrar o numero que vc digitar: ')
-numero_float = float(numero)
 
-print(f'o dobro e {numero} é {numero_float*2:.2f}')
+# if numero.isdigit():
+#     numero_float = float(numero)
+#     print(f'o dobro e {numero} é {numero_float*2:.2f}')
+# else:
+#     print('Isso não é um numero')
+
+try:
+    numero_float = float(numero)
+    print('FLOAT: ', numero_float)
+    print(f'o dobro e {numero} é {numero_float*2:.2f}')
+except:
+    print('Isso não é um numero')
+    
